cf3ecb2eae45037db87016c53e9ab24bd0aefa78
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2022-06-17 22:09:03 +0100

    Proof

--- src/Fin.v
+++ src/Fin.v
@@ -215,7 +215,7 @@ Section Fin.
 
   Fact uip_nat {n : nat} (e : n = n) : e = eq_refl.
   Proof. apply UIP_dec, eq_nat_dec. Qed.
-
+  
   Fact le_inv n m (H : n <= m) :
       (exists e : m = n, eq_rect _ _ H _ e = le_n n)
     \/ (exists m' (e : m = S m') (H' : n <= m'), 
@@ -223,40 +223,94 @@ Section Fin.
   Proof.
     revert m H.
     intros ? [ | m H ].
-    + left; now exists eq_refl.
-    + right; now exists m, eq_refl, H.
+    + left. now exists eq_refl.
+    + right. now exists m, eq_refl, H.
   Qed.
-
+ 
+  
   Require Import Equations.Prop.Equations.
   Set Equations With UIP.
   Scheme le_indd := Induction for le Sort Prop.
 
-  Lemma le_pair_ind :
-    forall (n : nat)
+  
+  Lemma le_unique : forall (m n : nat)
+    (p q : m <= n), p = q.
+  Proof.
+    intros ?. 
+    refine(fix Fn n p {struct p} :=
+      match p as p' in (_ <= np)
+        return n = np -> forall (q : m <= np), 
+          p' = q  
+      with 
+      | le_n _ => fun Heq q => 
+        match q as q' in (_ <= nq)
+          return forall (pf : nq = m),
+            le_n m = (eq_rect _ _ q' _ pf)
+        with
+        | le_n _ => fun pf => _ 
+        | le_S _ nt Hnt => fun pf => _ 
+        end  eq_refl
+      | le_S _ nt Hnt => fun Heq q => 
+        match q as q' in (_ <= S np)
+          return 
+            forall (pf : np = nt),
+              le_S _ nt Hnt =  
+              eq_rect np (fun w => m <= S w) q' nt pf  
+        with 
+        | le_n _ => _ 
+        | le_S _ nw Hnw => fun pf => _ 
+        end eq_refl
+      end eq_refl).
+    + rewrite (uip_nat pf).
+      exact eq_refl.
+    + abstract nia.
+    + clear Fn.
+      (* It was a hunch from Dominiques Solution but 
+      why do I need to clear it? *)
+      destruct m.
+      ++ exact idProp.
+      ++ abstract nia.
+    + subst; simpl.
+      f_equal.
+      apply Fn.
+  Qed.
+  
+
+
+  
+  Lemma le_pair_indd :
+    forall (n m : nat)
     (P : forall m : nat, n <= m -> n <= m -> Prop),
     P n (le_n n) (le_n n) ->
-    (forall (m : nat) (l h : n <= m), P m l h ->
-      P (S m) (le_S n m l) (le_S n m h)) ->
-    forall (m : nat) (l h : n <= m), P m l h.
+    (forall (m : nat) (Ha Hb : n <= m), P m Ha Hb ->
+      P (S m) (le_S n m Ha) (le_S n m Hb)) ->
+    forall (mt : nat) (Hna Hnb : n <= mt), P mt Hna Hnb.
   Proof.
-    intros ? P Hb Hc.
+    intros ? ? ? Pa Pb.
     refine(
-      fix Fn m l :=
-      match l as lp in (_ <= mp) return forall h, P mp lp h with
-      | le_n _  => _ 
-      | le_S _ m lp => _
-      end).
-    + intro h.
-      inversion h.
-      assert (Ht : h = le_n n \/ exists w, h = le_S _ _ h).
-      destruct (le_inv _ _ h) as [ (e & H) | (m' & e & H1 & H2) ].
-      ++ now rewrite (uip_nat e) in H.
-      ++ nia.
-    + intro h.
-      destruct (le_inv _ _ h) as [ (e & H) | (m' & e & H1 & H2) ].
-      ++ nia.
-      ++ injection e; intros H3.
-        revert e H1 H2; rewrite <- H3; intros e H1 H2.
+      fix Fn mt Hna :=
+      match Hna as Hna' in (_ <= mtp) 
+        return 
+          mt = mtp -> 
+          forall Hnb, P mtp Hna' Hnb
+      with 
+      | le_n _ => fun Hna Hnb => 
+        match Hnb as Hnb' in (_ <= nt) 
+          return 
+            forall (pf : nt = n),
+            P n (le_n n) (eq_rect nt _ Hnb' n pf)
+        with 
+        | le_n _ =>  fun pf => _ 
+        | le_S _ nt Hnt => fun pf => _  
+        end eq_refl
+      | le_S _ nt Hnt => _
+      end eq_refl).
+    + rewrite (uip_nat pf).
+      exact Pa.
+    + abstract nia.
+    + 
+      
+      
 
 
       
