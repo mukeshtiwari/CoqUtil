fe012ff4379a5584bfebd71efd8e45649a6f82e0
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-02-11 18:12:43 +0000

    Update Acc.v

--- src/Acc.v
+++ src/Acc.v
@@ -20,3 +20,41 @@ Proof.
       apply Ih.
       nia. 
 Qed.
+
+
+From Coq Require Import List Utf8 RelationClasses
+  PeanoNat Psatz Inverse_Image Wf_nat.
+
+
+
+Section Wf. 
+
+  Theorem well_founded_fn {A : Type} (P : A -> Prop) : forall (f : A -> nat),
+    (forall (a : A), (forall (b : A), f b < f a -> P b) -> P a) -> forall (a : A), P a.
+  Proof.
+    intros f Ha a.
+    refine ((fix Fn (u : A) (acc : Acc lt (f u)) {struct acc} : P u := 
+      match f u as u' return f u = u' -> _ with 
+      | 0 => fun (Hb : f u = 0) => Ha _ (fun (b : A) (Hc : f b < f u) => _ ) 
+      | S n => fun Hb => 
+        match acc with 
+        | Acc_intro _ R => Ha u (fun (b : A) 
+          (Hc : f b < f u) => Fn b (R (f b) Hc))
+        end
+      end eq_refl) a _); [abstract nia | eapply lt_wf].
+  Qed.
+
+  Theorem well_founded_acc {A : Type} (P : A -> Prop) : forall (f : A -> nat),
+    (forall (a : A), (forall (b : A), f b < f a -> P b) -> P a) -> 
+    forall (a : A), P a.
+  Proof.
+    intros f Ha a.
+    refine((fix Fn (u : A) (acc : Acc lt (f u)) : P u := 
+      match acc with 
+      | Acc_intro _ R => 
+        Ha u (λ (b : A) (Hb : f b < f u), Fn b (R (f b) Hb))
+      end) a _); eapply lt_wf.
+  Qed.
+
+
+End Wf. 
