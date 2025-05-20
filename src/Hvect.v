19fd4eec5fbb3408cc8de15b0b32c48455ee224c
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-05-20 19:22:49 +0100

    Update Hvect.v

--- src/Hvect.v
+++ src/Hvect.v
@@ -39,12 +39,58 @@ Section Hvect.
              match hv in Hvect n' v' return forall (f : Fin.t n'), v'[@f] with
              | Hnil => fun f => match f with end 
              | @Hcons _ n' _ hvh hvt => fun (f : Fin.t (S n')) => _ 
+                                                                     
              end).
     destruct (fin_inv _ f) as [ha | (f' & ha)]; subst; cbn;
       [exact hvh | eapply fn; exact hvt].
+    Show Proof.
   Defined.
 
   
   Eval compute in hvect_nth_fin (Hcons true (Hcons 1 Hnil)) (FS F1).
 
-End Hvect. 
+  
+  Definition hvect_nth {n : nat}  
+    (f : Fin.t n) {v : Vector.t Type n} (u : Hvect n v) : Vector.nth v f.
+  Proof.
+    generalize dependent n.
+    refine(fix fn {n : nat} (f : Fin.t n) {struct f} : 
+      ∀ (v : Vector.t Type n), Hvect n v → v[@f] := 
+      match f as f' in Fin.t n' return ∀ (v : Vector.t Type n'), 
+        Hvect n' v → v[@f']
+      with 
+      | Fin.F1 => fun v hv => _ 
+      | Fin.FS f => fun v hv => _ 
+      end).
+    +     
+      refine 
+        (match hv in Hvect n' v' return
+              (match n' as n'' return Vector.t Type n'' -> Type
+               with
+               | 0 => fun _ => IDProp
+               | S m' => fun (e : Vector.t Type (S m')) => e[@F1]
+               end v')    
+        with 
+        | Hcons hvh hvt => hvh
+        end).
+    + 
+      refine
+        (match hv in Hvect n' v' return forall (pf : n' = S n0), 
+               (match n' as n'' return
+                      Fin.t (Nat.pred n'') -> Vector.t Type n'' -> Type
+                with
+                | 0 => fun _ _ => IDProp
+                | S m' => fun (ea : Fin.t m') (v : Vector.t Type (S m')) =>
+                           v[@FS ea]
+                end (eq_rec_r (λ np : nat, t (Nat.pred np))
+                       (f : t (Nat.pred (S n0))) pf) v')
+         with
+         | Hnil => fun _ => idProp
+         | Hcons hvh hvt => fun pf => _ 
+         end eq_refl); cbn.
+      eapply fn.
+      exact hvt.
+  Defined.
+
+
+End Hvect.
