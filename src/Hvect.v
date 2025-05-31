598c559a687522ef53ba6abdb21431b58738be2c
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-05-31 19:16:50 +0100

    Update Hvect.v

--- src/Hvect.v
+++ src/Hvect.v
@@ -126,6 +126,104 @@ Section UIP.
       end).
   Defined.
 
+  Theorem le_unique_fail : ∀ (n m : nat) (ha hb : le n m), ha = hb.
+  Proof.
+    refine(fix fn (n m : nat) (ha : le n m) {struct ha} : 
+      ∀ (hb : le n m), ha = hb := 
+      match ha as ha' in le _ m' return ∀(pfa : m = m'), 
+        ha = @eq_rect nat m' (fun w => le n w) ha' m (eq_sym pfa) -> 
+        ∀ (hb : le n m'), ha' = hb 
+      with
+      | le_n _ =>  _ 
+      | le_S _ m' pfb => _ 
+      end eq_refl eq_refl).
+    +
+      intros * hb hc.
+      generalize dependent hc.
+      refine(fun hc => 
+        match hc as hc' in _ ≤ n' return ∀ (pfb : n = n'), 
+          le_n n' = @eq_rect _ n (fun w => w ≤ n') hc' n' pfb  
+        with 
+        | le_n _ => _ 
+        | le_S _ nw pfb => _ 
+        end eq_refl).     
+        ++
+          intros *. 
+          assert (hd : pfb = eq_refl) by 
+          (apply uip_nat).
+          subst; cbn; exact eq_refl. 
+        ++
+          intros pfc. subst.
+          abstract nia.  
+    +
+      intros * hb hc.
+      generalize dependent pfb.
+      generalize dependent pfa.
+      generalize dependent hc.
+      refine(fun hc => 
+        match hc as hc' in _ ≤ (S mp) return ∀ (pf : mp = m') pfa pfb hb,
+          le_S n mp (@eq_rect _ m' (fun w => n ≤ w) pfb mp (eq_sym pf)) = hc'
+        with 
+        | le_n _ => _ 
+        | le_S _ nw pfc => _
+        end eq_refl).
+      ++
+        destruct n as [| n].
+        +++ exact idProp.
+        +++
+          intros * hb. abstract nia.
+      ++
+        intros * hb. subst; cbn.
+        f_equal. eapply fn. 
+        Fail Guarded.
+  Admitted.
+
+  Theorem le_unique_didnot_fail : ∀ (n m : nat) (ha hb : le n m), ha = hb.
+  Proof.
+    refine(fix fn (n m : nat) (ha : le n m) {struct ha} : 
+      ∀ (hb : le n m), ha = hb := 
+      match ha as ha' in le _ m' return ∀(pfa : m = m'), 
+        ha = @eq_rect nat m' (fun w => le n w) ha' m (eq_sym pfa) -> 
+        ∀ (hb : le n m'), ha' = hb 
+      with
+      | le_n _ =>  _ 
+      | le_S _ m' pfb => _ 
+      end eq_refl eq_refl).
+    +
+      intros * hb hc.
+      refine(match hc as hc' in _ ≤ n' return ∀ (pfb : n = n'), 
+          le_n n' = @eq_rect _ n (fun w => w ≤ n') hc' n' pfb  
+        with 
+        | le_n _ => _ 
+        | le_S _ nw pfb => _ 
+        end eq_refl).     
+        ++
+          intros *. 
+          assert (hd : pfb = eq_refl) by 
+          (apply uip_nat).
+          subst; cbn; exact eq_refl. 
+        ++
+          intros pfc. subst.
+          abstract nia.  
+    +
+      intros * hb hc.
+      refine(match hc as hc' in _ ≤ (S mp) return ∀ (pf : mp = m'),
+          le_S n mp (@eq_rect _ m' (fun w => n ≤ w) pfb mp (eq_sym pf)) = hc'
+        with 
+        | le_n _ => _ 
+        | le_S _ nw pfc => _
+        end eq_refl).
+      ++
+        destruct n as [| n].
+        +++ exact idProp.
+        +++
+          intros *. abstract nia.
+      ++
+        intros *. subst; cbn.
+        f_equal. eapply fn.
+  Defined.
+  
+  
   Theorem le_unique : ∀ (n m : nat) (ha hb : le n m), ha = hb.
   Proof.
     refine(fix fn (n m : nat) (ha : le n m) {struct ha} : 
@@ -171,13 +269,14 @@ Section UIP.
         intros * hb. subst. 
         abstract nia.
       ++
-        intros * hb.  
+        intros * hb.   
         inversion pf as [hpf]; subst.
         assert (hd : pf = eq_refl) by 
         (apply uip_nat). 
         subst; cbn. 
-        f_equal. eapply fn.
+        f_equal; eapply fn.
   Defined.
+
      
 End UIP. 
 
