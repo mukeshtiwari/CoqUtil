579c41eba0faa61ba6765f9ddc62b8d0a50eed42
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-05-31 14:41:04 +0100

    Update Fail.v

--- Fail.v
+++ Fail.v
@@ -60,7 +60,7 @@
                                 eq_rect n0 (λ w : nat, w ≤ S nw) 
                                   (le_S n0 nw pfb0) (S nw) pfc0)
                          (λ (hc1 : S nw ≤ S nw) (pfb0 : S nw ≤ nw),
-                            le_unique_fail_subproof nw pfb0 hc1)
+                            le_unique_subproof nw pfb0 hc1)
                          pfc hc0 pfb)
                     pfa ha hb
             end eq_refl)
@@ -72,129 +72,144 @@
          (hc : n ≤ S m'),
          (λ hc0 : n ≤ S m',
             match
-              hc0 as hc' in (_ ≤ iV)
+              hc0 as hc' in (_ ≤ mp)
               return
-                (match iV as iV0 return (n ≤ iV0 → Prop) with
-                 | 0 => λ _ : n ≤ 0, IDProp
-                 | S mp =>
-                     λ hc'0 : n ≤ S mp,
-                       ∀ (pf : mp = m') (pfa0 : m = S m') (pfb0 : n ≤ m'),
-                         ha =
-                         eq_rect (S m') (λ w : nat, n ≤ w) 
-                           (le_S n m' pfb0) m (eq_sym pfa0)
-                         → le_S n mp
-                             (eq_rect m' (λ w : nat, n ≤ w) pfb0 mp (eq_sym pf)) =
-                           hc'0
-                 end hc')
+                (∀ (pf : mp = S m') (pfa0 : m = S m') (pfb0 : n ≤ m'),
+                   ha =
+                   eq_rect (S m') (λ w : nat, n ≤ w) (le_S n m' pfb0) m
+                     (eq_sym pfa0)
+                   → le_S n m' pfb0 = eq_rect mp (le n) hc' (S m') pf)
             with
             | le_n _ =>
-                match
-                  n as n0
-                  return
-                    (∀ ha0 : n0 ≤ m,
-                       n0 ≤ S m'
-                       → match n0 as iV return (n0 ≤ iV → Prop) with
-                         | 0 => λ _ : n0 ≤ 0, IDProp
-                         | S mp =>
-                             λ hc' : n0 ≤ S mp,
-                               ∀ (pf : mp = m') (pfa0 : m = S m') (pfb0 : n0 ≤ m'),
-                                 ha0 =
-                                 eq_rect (S m') (λ w : nat, n0 ≤ w)
-                                   (le_S n0 m' pfb0) m 
-                                   (eq_sym pfa0)
-                                 → le_S n0 mp
-                                     (eq_rect m' (λ w : nat, n0 ≤ w) pfb0 mp
-                                        (eq_sym pf)) =
-                                   hc'
-                         end (le_n n0))
-                with
-                | 0 => λ (_ : 0 ≤ m) (_ : 0 ≤ S m'), idProp
-                | S n0 =>
-                    (λ (n1 : nat) (ha0 : S n1 ≤ m) (hc1 : S n1 ≤ S m') 
-                       (pf : n1 = m') (pfa0 : m = S m') 
-                       (pfb0 : S n1 ≤ m') (_ : ha0 =
-                                               eq_rect 
-                                                 (S m') 
-                                                 (λ w : nat, S n1 ≤ w)
-                                                 (le_S (S n1) m' pfb0) m
-                                                 (eq_sym pfa0)),
-                       le_unique_fail_subproof0 n1 m ha0 m' hc1 pf pfa0 pfb0)
-                      n0
-                end ha hc0
-            | le_S _ nw pfc =>
-                λ (pf : nw = m') (pfa0 : m = S m') (pfb0 : n ≤ m') 
+                λ (pf : n = S m') (pfa0 : m = S m') (pfb0 : n ≤ m') 
                   (hb0 : ha =
                          eq_rect (S m') (λ w : nat, n ≤ w) 
                            (le_S n m' pfb0) m (eq_sym pfa0)),
+                  EqdepFacts.internal_eq_rew_r_dep
+                    (λ (n0 : nat) (pf0 : n0 = S m'),
+                       ∀ ha0 : n0 ≤ m,
+                         n0 ≤ S m'
+                         → ∀ pfb1 : n0 ≤ m',
+                             ha0 =
+                             eq_rect (S m') (λ w : nat, n0 ≤ w) 
+                               (le_S n0 m' pfb1) m (eq_sym pfa0)
+                             → le_S n0 m' pfb1 =
+                               eq_rect n0 (le n0) (le_n n0) (S m') pf0)
+                    (λ (ha0 : S m' ≤ m) (hc1 : S m' ≤ S m') 
+                       (pfb1 : S m' ≤ m') (hb1 : ha0 =
+                                                 eq_rect 
+                                                   (S m') 
+                                                   (λ w : nat, S m' ≤ w)
+                                                   (le_S (S m') m' pfb1) m
+                                                   (eq_sym pfa0)),
+                       EqdepFacts.internal_eq_rew_r_dep
+                         (λ (m0 : nat) (pfa1 : m0 = S m'),
+                            ∀ ha1 : S m' ≤ m0,
+                              ha1 =
+                              eq_rect (S m') (λ w : nat, S m' ≤ w)
+                                (le_S (S m') m' pfb1) m0 
+                                (eq_sym pfa1)
+                              → le_S (S m') m' pfb1 =
+                                eq_rect (S m') (le (S m')) 
+                                  (le_n (S m')) (S m') eq_refl)
+                         (λ (ha1 : S m' ≤ S m') (_ : ha1 =
+                                                     eq_rect 
+                                                     (S m') 
+                                                     (λ w : nat, S m' ≤ w)
+                                                     (le_S (S m') m' pfb1) 
+                                                     (S m') 
+                                                     (eq_sym eq_refl)),
+                            le_unique_subproof0 m' hc1 pfb1)
+                         pfa0 ha0 hb1)
+                    pf ha hc0 pfb0 hb0
+            | le_S _ nw pfc =>
+                λ (pf : S nw = S m') (pfa0 : m = S m') 
+                  (pfb0 : n ≤ m') (hb0 : ha =
+                                         eq_rect (S m') 
+                                           (λ w : nat, n ≤ w) 
+                                           (le_S n m' pfb0) m 
+                                           (eq_sym pfa0)),
                   let H :
                     (λ n0 : nat,
-                       n0 = m'
-                       → le_S n nw
-                           (eq_rect m' (λ w : nat, n ≤ w) pfb0 nw (eq_sym pf)) =
-                         le_S n nw pfc)
-                      m' :=
+                       n0 = S m'
+                       → le_S n m' pfb0 =
+                         eq_rect (S nw) (le n) (le_S n nw pfc) (S m') pf)
+                      (S m') :=
                     match
                       pf as _ in (_ = n0)
                       return
-                        (n0 = m'
-                         → le_S n nw
-                             (eq_rect m' (λ w : nat, n ≤ w) pfb0 nw (eq_sym pf)) =
-                           le_S n nw pfc)
+                        (n0 = S m'
+                         → le_S n m' pfb0 =
+                           eq_rect (S nw) (le n) (le_S n nw pfc) (S m') pf)
                     with
                     | eq_refl =>
-                        λ H : nw = m',
-                          (λ hpf : nw = m',
-                             EqdepFacts.internal_eq_rew_r_dep
-                               (λ (nw0 : nat) (pf0 : nw0 = m'),
-                                  ∀ pfc0 : n ≤ nw0,
-                                    nw0 = m'
-                                    → le_S n nw0
-                                        (eq_rect m' (λ w : nat, n ≤ w) pfb0 nw0
-                                           (eq_sym pf0)) =
-                                      le_S n nw0 pfc0)
-                               (λ (pfc0 : n ≤ m') (hpf0 : m' = m'),
-                                  EqdepFacts.internal_eq_rew_r_dep
-                                    (λ (m0 : nat) (pfa1 : m0 = S m'),
-                                       ∀ ha0 : n ≤ m0,
-                                         ha0 =
-                                         eq_rect (S m') 
-                                           (λ w : nat, n ≤ w) 
-                                           (le_S n m' pfb0) m0 
-                                           (eq_sym pfa1)
-                                         → le_S n m'
-                                             (eq_rect m' 
-                                                (λ w : nat, n ≤ w) pfb0 m'
-                                                (eq_sym eq_refl)) =
-                                           le_S n m' pfc0)
-                                    (λ (ha0 : n ≤ S m') 
-                                       (_ : ha0 =
-                                            eq_rect (S m') 
-                                              (λ w : nat, n ≤ w) 
-                                              (le_S n m' pfb0) 
-                                              (S m') (eq_sym eq_refl)),
-                                       let hd : hpf0 = eq_refl := 
-                                         uip_nat m' hpf0 in
-                                       (let H0 : pfb0 = pfc0 := 
-                                          fn n m' pfb0 pfc0 in
-                                        (let H1 : m' = m' := eq_refl in
-                                         (let H2 : n = n := eq_refl in
-                                          (λ (_ : n = n) 
-                                             (_ : m' = m') 
-                                             (H3 : pfb0 = pfc0),
-                                             eq_trans
-                                               (f_equal 
-                                                  (λ f : ..., f pfb0) eq_refl)
-                                               (f_equal (le_S n m') H3))
-                                            H2)
-                                           H1)
-                                          H0)
-                                       :
-                                       le_S n m'
-                                         (eq_rect m' (λ w : nat, n ≤ w) pfb0 m'
-                                            (eq_sym eq_refl)) =
-                                       le_S n m' pfc0)
-                                    pfa0 ha hb0)
-                               pf pfc hpf)
+                        λ H : S nw = S m',
+                          (λ hpf : S nw = S m',
+                             let H0 : nw = m' :=
+                               f_equal
+                                 (λ e : nat,
+                                    match e with
+                                    | 0 => nw
+                                    | S n0 => n0
+                                    end)
+                                 hpf
+                               in
+                             (λ hpf0 : nw = m',
+                                EqdepFacts.internal_eq_rew_r_dep
+                                  (λ (m0 : nat) (pfa1 : m0 = S m'),
+                                     ∀ ha0 : n ≤ m0,
+                                       ha0 =
+                                       eq_rect (S m') 
+                                         (λ w : nat, n ≤ w) 
+                                         (le_S n m' pfb0) m0 
+                                         (eq_sym pfa1)
+                                       → le_S n m' pfb0 =
+                                         eq_rect (S nw) 
+                                           (le n) (le_S n nw pfc) 
+                                           (S m') pf)
+                                  (λ (ha0 : n ≤ S m') 
+                                     (_ : ha0 =
+                                          eq_rect (S m') 
+                                            (λ w : nat, n ≤ w) 
+                                            (le_S n m' pfb0) 
+                                            (S m') (eq_sym eq_refl)),
+                                     eq_ind_r
+                                       (λ nw0 : nat,
+                                          ∀ (pfc0 : n ≤ nw0) (pf0 : S nw0 = S m'),
+                                            le_S n m' pfb0 =
+                                            eq_rect (S nw0) 
+                                              (le n) (le_S n nw0 pfc0) 
+                                              (S m') pf0)
+                                       (λ (pfc0 : n ≤ m') (pf0 : S m' = S m'),
+                                          let hd : pf0 = eq_refl :=
+                                            uip_nat (S m') pf0 in
+                                          eq_ind_r
+                                            (λ pf1 : S m' = S m',
+                                               le_S n m' pfb0 =
+                                               eq_rect 
+                                                 (S m') 
+                                                 (le n) 
+                                                 (le_S n m' pfc0) 
+                                                 (S m') pf1)
+                                            ((let H1 : 
+                                                pfb0 = pfc0 := 
+                                                fn n m' pfb0 pfc0 in
+                                              (let H2 : m' = m' := eq_refl in
+                                               (let H3 : n = n := eq_refl in
+                                                (λ ... ... ..., eq_trans ... ...)
+                                                  H3)
+                                                 H2)
+                                                H1)
+                                             :
+                                             le_S n m' pfb0 =
+                                             eq_rect (S m') 
+                                               (le n) 
+                                               (le_S n m' pfc0) 
+                                               (S m') eq_refl)
+                                            hd)
+                                       hpf0 pfc pf)
+                                  pfa0 ha hb0)
+                               H0)
                             H
                     end in
                   H eq_refl
