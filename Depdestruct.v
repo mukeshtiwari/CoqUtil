03ae1d504ccee2bdf2597b89bc4262ef8646af28
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-06-17 11:39:41 +0100

    Update Depdestruct.v

--- Depdestruct.v
+++ Depdestruct.v
@@ -27,3 +27,29 @@ Proof.
   clearbody hc.
   rewrite hb in ha.
   destruct o.
+
+
+(* another one *)
+
+ 
+Lemma some_DepRec_lemma (r : DepRec) (f : DepRec -> Prop) : f r.
+Proof.
+  destruct r as [r ha].
+  unfold someListProp in *.
+  generalize (eq_refl (nth_error r 10)).
+  generalize (nth_error r 10) at 2.
+  intros * hb.
+  set (fn := fun (o : option nat) =>
+    match o with 
+    | Some _ => True
+    | None => False
+  end).
+  set(hc := (eq_rect _ fn ha _ hb)).
+  assert (hd : ha = eq_rect o fn hc (nth_error r 10) (eq_sym hb)).
+  subst hc. rewrite rew_compose, eq_trans_sym_inv_r.
+  cbn; reflexivity.
+  replace ha with (eq_rect o fn hc (nth_error r 10) (eq_sym hb)).
+  clearbody hc. clear hd. 
+  rewrite hb in ha.
+  destruct o.
+  
