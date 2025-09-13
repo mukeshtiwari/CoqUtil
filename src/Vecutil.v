3a6d65bb046633703fea39faefde91d5524e5d8e
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-09-13 08:36:38 +0100

    Refactor tail and map2vec functions for clarity

--- src/Vecutil.v
+++ src/Vecutil.v
@@ -1,4 +1,3 @@
-(* code from Thomas Lamiaux *)
 Inductive vec (A : Type) : nat -> Type :=
 | vnil : vec A 0
 | vcons : A -> forall n, vec A n -> vec A (S n).
@@ -21,39 +20,26 @@ Fail Fixpoint map2vec {A B C : Set} (f : A -> B -> C) n (vA : vec A n) (vB : vec
 
 (* opti in the elaboration *)
 Definition tail {A} n (v : vec A (S n)) : vec A n :=
-  match v as v' in vec _ n' return 
-    match n' return Type 
-    with 
-    | 0 =>  IDProp 
-    | S n'' => vec A n''
-    end  
-  with
+  match v as x in vec _ (S n) return vec A n with
   | vcons _ _ v => v
   end.
-Print tail.
 
 Print tail.
 Print IDProp.
 
-
 (* work thanks to beta-iota for vB *)
 Definition map2vec1 {A B C : Set} (f : A -> B -> C) : forall n, vec A n -> vec B n -> vec C n :=
-  fix fn (n : nat) (vA : vec A n) (vB : vec B n) {struct vB} : vec C n :=
+  fix rec (n : nat) (vA : vec A n) (vB : vec B n) {struct vB} : vec C n :=
   match vA in vec _ k return vec B k -> vec C k with
     | vnil => fun (_ : vec B 0) => vnil
-    | vcons a k tA => 
+    | vcons a k tA => (
       (* tA : vec A k *)
       fun vB : vec B (S k) =>
-      match vB in vec _ p return 
-        (match p return Type 
-        with 
-        | 0 => True 
-        | S p => vec A p -> vec C (S p) 
-        end) 
-      with
+      match vB in vec _ p return (match p with 0 => True | S p => vec A p -> vec C (S p) end) with
       | vnil => I
-      | vcons b p tB => fun tA => vcons (f a b) p (fn p tA tB)
+      | vcons b p tB => fun tA => vcons (f a b) p (rec p tA tB)
       end tA
+    )
   end vB.
 
 (* no beta-iota for tA => obfuscated by the match *)
@@ -65,7 +51,7 @@ Fail Definition map2vec2 {A B C : Set} (f : A -> B -> C) : forall n, vec A n -> 
       fun vB : vec B (S k) =>
       match vB in vec _ p return (match p with 0 => True | S p => vec A p -> vec C (S p) end) with
       | vnil => I
-      | vcons b p tB => fun ta => vcons (f a b) p (rec p tA tB)
+      | vcons b p tB => fun tA => vcons (f a b) p (rec p tA tB)
       end tA
     )
   end.
@@ -76,13 +62,7 @@ Definition map2vec3 {A B C : Set} (f : A -> B -> C) : forall n, vec A n -> vec B
     | vnil => fun (_ : vec B 0) => vnil
     | vcons a k tA => (
       fun vB : vec B (S k) =>
-      match vB in vec _ p return 
-        (vec A 
-          (match p with 
-          | 0 => 0 
-          | S p' => p' 
-          end) -> vec C p) 
-      with
+      match vB in vec _ p return (vec A (match p with 0 => 0 | S p => p end) -> vec C p) with
       | vnil => fun _ => vnil
       | vcons b p tB => fun tA => vcons (f a b) p (rec p tA tB)
       end tA
@@ -94,7 +74,7 @@ Definition map2vec4 {A B C : Set} (f : A -> B -> C) : forall n, vec A n -> vec B
   fix rec (n : nat) (vA : vec A n) {struct vA} : vec B n -> vec C n :=
   match vA in vec _ k return vec B k -> vec C k with
     | vnil => fun (_ : vec B 0) => vnil
-    | vcons a k tA => (fun vB : vec B (S k) =>
+    | vcons a k tA => ( fun vB : vec B (S k) =>
       match vB in vec _ p return (match p with 0 => True | S p => (vec B p -> vec C p) -> vec C (S p) end) with
       | vnil => I
       | vcons b p tB => fun rec' => vcons (f a b) p (rec' tB)
